3f20770bc35f5142d965641cb1839f32942a914f
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-22 13:39:02 +0530

    asdas

--- brigade.js
+++ brigade.js
@@ -2,9 +2,6 @@ const { events, Job } = require("brigadier");
 events.on("push", () => {
 var job = new Job("buildfinal", "docker:dind");
 job.privileged = true;
-job.env = {
-DOCKER_DRIVER: "overlay"
-}
 job.tasks = [
 // "dockerd &",
 "dockerd-entrypoint.sh &",
