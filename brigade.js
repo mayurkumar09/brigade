57f5ed1a78781a3755bf19808b5ba3318ca6d89a
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-22 12:58:15 +0530

    dsfsd

--- brigade.js
+++ brigade.js
@@ -1,15 +1,15 @@
 const { events, Job } = require("brigadier");
 events.on("push", () => {
-  var job = new Job("buildfinal", "docker:dind");
-  job.privileged = true;
-  job.tasks = [
-      "dockerd &",
-      "cd /src",
-      "ls -l",
-      "docker info",
-      "sleep 120",
-      "docker ps"
-  ];
-
-  job.run();
-});
+var job = new Job("buildfinal", "docker:dind");
+job.privileged = true;
+dockerBuild.env = {
+"DOCKER_DRIVER": "overlay"
+}
+job.tasks = [
+"dockerd &",
+"cd /src",
+"ls -l",
+"docker info",
+"sleep 10",
+"docker ps"
+];
