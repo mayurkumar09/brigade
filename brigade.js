de11284f94f9adc86724009d2bdab39fc64278b0
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-22 10:30:40 +0530

    sdfsd

--- brigade.js
+++ brigade.js
@@ -1,17 +1,16 @@
 const { events, Job } = require("brigadier");
 events.on("push", () => {
-  var job = new Job("buildfinal", "docker:stable-dind");
+  var job = new Job("buildfinal", "docker:dind");
   job.privileged = true;
-   
+
   job.tasks = [
       "cd /src",
       "ls -l",
-      "docker run --rm -it --privileged docker:stable-dind",
       "docker info",
       "dockerd &",
       "sleep 20",
       "docker ps"
   ];
-  job.docker.enabled = true;
+
   job.run();
 });
