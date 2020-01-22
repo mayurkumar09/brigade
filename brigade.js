fee0b4eee4f2157a258239a7b076b70b95e68063
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-22 10:46:47 +0530

    go

--- brigade.js
+++ brigade.js
@@ -2,7 +2,6 @@ const { events, Job } = require("brigadier");
 events.on("push", () => {
   var job = new Job("buildfinal", "docker:dind");
   job.privileged = true;
-
   job.tasks = [
       "dockerd &",
       "cd /src",
